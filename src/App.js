bfd2e52b024f934c592f604454e2bb5161dfdf10
Vishesh Baghel <71141511+vishesh-baghel@users.noreply.github.com>
2022-02-24 12:39:40 +0530

    Basic routing completed

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import CreatePost from "./pages/CreatePost";
 
 function App() {
   return (
     <Router>
+      <nav>
+        <Link to="/">Home</Link>
+        <Link to="/CreatePost">Create Post</Link>
+      </nav>
       <Routes>
-        <route></route>
+        <Route path="/" element={<Home />} />
+        <Route path="/Login" element={<Login />} />
+        <Route path="/CreatePost" element={<CreatePost />} />
       </Routes>
     </Router>
   );
